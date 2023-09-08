4ff7ea930d7a8b16b6df7ecd56dacc0695f8ebe1
AslamThachapalli <aslam.develop912@gmail.com>
2023-09-08 22:54:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Home from "./pages/Home";
+import Achievements from "./pages/Achievements";
+import Info from "./pages/Info";
+import Skills from "./pages/Skills";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Achievements />
+      <Skills />
+      <Info />
+    </>
+  );
+}
+
+export default App;
